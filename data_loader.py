c007a75006c44ae6333ad98376453435b1dc746a
Dhawal Dixit <dhawal.p.dixit@gmail.com>
2026-03-03 22:17:02 -0600

    changes to data loading func, we take query params and query in duck_db load now

--- data_loader.py
+++ data_loader.py
@@ -16,7 +16,7 @@ class DataLoader():
     def parallel_load(self, fn, shards=0, max_workers=None, batch_size=10, **kwargs) -> pd.DataFrame:
         '''
         function to run another function (that returns a dataframe) across concurrent futures.
-            args : 
+            args :
                 fn : function which will be executed by concurrent futures
                 shards : number of shards we want to read from the gcs bucket
                 max-workers : number of parallel workers
@@ -37,41 +37,80 @@ class DataLoader():
         return pd.concat(results, ignore_index=True)
 
 
-    def dck_db_load(self, shard_range_start=None, shard_range_end=None, cols=None, collection_ids=None, instance_indexes=None) -> pd.DataFrame:
+    def dck_db_load(
+                      self,
+                      shard_range_start=None,
+                      shard_range_end=None,
+                      query=None,
+                      cols=None,
+                      target_collection_ids=None,
+                      target_instance_indexes=None,
+                      target_pairs=None
+                      ) -> pd.DataFrame:
 
         '''
-        function to initialize a duckdb connection and query shards between 'shard_range_start' and 'shard_range_end' from gcs bucket. 
+        function to initialize a duckdb connection and query shards between 'shard_range_start' and 'shard_range_end' from gcs bucket.
         args:
-            shard_range_start : start reading shards from here 
+            shard_range_start : start reading shards from here
             shard_range_end : stop reading here
+            query : string containing the query to run
             cols : string containing comma separated cols to pull (goes inside the query)
-            collection_ids : string containing comma separated collection ids to filter on (goes inside the query)
-            instance_indexes : string containing comma separated instance idxs to filter on (goes inside the query) // this might probably be removed
+            target_collection_ids : string containing comma separated collection ids to filter on (goes inside the query)
+            target_instance_indexes : string containing comma separated instance idxs to filter on (goes inside the query) // this might probably be removed
+            target_pairs : string containing comma separated pairs of (collection_id, instance_index) to filter on (goes inside the query)
         '''
         assert shard_range_start is not None, "shard_range_start cannot be None"
         assert shard_range_end is not None, "shard_range_end cannot be None"
+        assert query is not None, "query cannot be None"
         assert cols is not None, "cols cannot be None"
-        assert collection_ids is not None, "collection_ids cannot be None"
-        assert instance_indexes is not None, "instance_indexes cannot be None"
+        if (query == 'instance_usage') | (query == 'collection_events'):
+          assert target_collection_ids is not None, "collection_ids cannot be None"
+        if query == 'instance_usage':
+          assert target_instance_indexes is not None, "instance_indexes cannot be None"
+        if query == 'instance_usage_1':
+          assert target_pairs is not None, "instance_pairs cannot be None"
 
         con = duckdb.connect()
         con.sql("INSTALL httpfs; LOAD httpfs;")
         con.sql(f"SET threads = {self.threads};")
+        char = str(shard_range_start).zfill(5);
+        query_set = {
+            'collection_events' : f"""
+                    SELECT {cols}
+                    FROM read_parquet('{self.base_url}/collection_events-0000000{char}.parquet.gz')
+                    WHERE collection_id IN ({target_collection_ids})
+                    LIMIT 10000
+                """,
+            'instance_events' : f"""
+                    SELECT {cols}
+                    FROM read_parquet('{self.base_url}/instance_events-0000000{char}.parquet.gz')
+                    WHERE collection_id IN ({target_collection_ids})
+                    --LIMIT 10000
+                """,
+            'instance_usage' : f"""
+                    SELECT {cols}
+
+                    FROM read_parquet('{self.base_url}/instance_usage-0000000{char}.parquet.gz')
+                    WHERE collection_id IN ({target_collection_ids})
+                    AND instance_index IN ({target_instance_indexes})
+                    ORDER BY collection_id, instance_index, start_time
+                    --LIMIT 10000
+                """,
+            'instance_usage_1' : f"""
+                    SELECT {cols}
+                    FROM read_parquet('{self.base_url}/instance_usage-0000000{char}.parquet.gz')
+                    WHERE (collection_id, instance_index) IN ({target_pairs})
+                    ORDER BY collection_id, instance_index, start_time
+                    --LIMIT 10000"""
+                }
 
         try:
             dfs=[]
             print(f"processing {shard_range_start} to {shard_range_end}...")
             for char in [str(i).zfill(5) for i in range(shard_range_start,shard_range_end+1)]:
                 print(f"processing {char}...")
-                df = con.sql(f"""
-                    SELECT {cols}
-
-                    FROM read_parquet('{self.base_url}/instance_usage-0000000{char}.parquet.gz')
-                    WHERE collection_id IN ({collection_ids})
-                    AND instance_index IN ({instance_indexes})
-                    ORDER BY collection_id, instance_index, start_time
-                    --LIMIT 10000
-                """).df()
+                #change query here
+                df = con.sql(query_set[query]).df()
                 dfs.append(df)
         finally:
             con.close()
